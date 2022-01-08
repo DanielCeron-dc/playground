b1af4c3fbceec49390813dfb114d028b85720469
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-08 13:45:13 -0500

    innecessary stuff deleted :p

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { greet } from "./tools/weirdthings";
 
 
 const askforName = async () => {
-	const name: string = (await readFromConsole("What is your name? ")) as string;
+	const name: string = await readFromConsole("What is your name? ");
 	console.log(greet(name));
 }
 
