037bc98efcb3d777777269fca1f109a9385ea869
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-08 12:12:04 -0500

    happy merge maded :D

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { readFromConsole } from "tools/readFromConsole"
+import { greet } from "tools/weirdthings";
 
 const response: string = readFromConsole("What is your name?");
-console.log("Hello " + response);
+console.log(greet(response));
