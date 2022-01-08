8efa12026c025f446b55d15d2162787c361eac5c
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-08 13:39:15 -0500

    new changes in master

--- src/tools/readFromConsole.ts
+++ src/tools/readFromConsole.ts
@@ -1,17 +1,17 @@
 
 import * as readline from "readline";
 
-export const readFromConsole = (message: string): string => {
+export const readFromConsole = async (message: string): Promise<string> => {
 
     const rd1 = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    rd1.question(message, (answer) => {
-        rd1.close();
-        return answer;
-    });
-
-    return "";
+    return new Promise<string>((resolve) => {
+        rd1.question(message, (answer) => {
+            resolve(answer);
+            rd1.close();
+        });
+    })
 }; 
