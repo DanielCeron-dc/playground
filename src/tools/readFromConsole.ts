028bd8aa1400704269fe448d09c9a0aac1f61ed9
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-08 13:37:11 -0500

    new changes

--- src/tools/readFromConsole.ts
+++ src/tools/readFromConsole.ts
@@ -1,17 +1,25 @@
 
 import * as readline from "readline";
 
-export const readFromConsole = (message: string): string => {
+export const readFromConsole = async (message: string, convertToNumber?: boolean): Promise<string | number> => {
 
     const rd1 = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    rd1.question(message, (answer) => {
-        rd1.close();
-        return answer;
-    });
+    return new Promise((resolve) => {
+        rd1.question(message, (answer: string) => {
+            if (convertToNumber) {
+                try {
+                    resolve(parseInt(answer));
+                } catch (error) {
+                    throw new Error("the value that was entered is not a number");
+                }
+                return;
+            }
+            resolve(answer);
 
-    return "";
+        });
+    });
 }; 
