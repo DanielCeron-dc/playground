4fbc8bec820c26dee2bdbe50cef5532d210845ba
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-08 11:53:04 -0500

    greetings improved :D

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world'); 
+import readline from "readline"; 
+
+const rd1 = readline.createInterface({
+	input: process.stdin, 
+	output: process.stdout
+}); 
+
+rd1.question("whats your name ?" , (answer) => {
+	console.log("hello " answer); 
+	rd1.close(); 
+}); 
