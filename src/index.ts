1e716e567325d26e990946433898ddfbddd38c10
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-08 12:28:25 -0500

    bugs fixed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { readFromConsole } from "tools/readFromConsole"
-import { greet } from "tools/weirdthings";
+import { readFromConsole } from "./tools/readFromConsole"
+import { greet } from "./tools/weirdthings";
 
 const response: string = readFromConsole("What is your name?");
 console.log(greet(response));
