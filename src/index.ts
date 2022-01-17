560ff5e4f009f62240f5b622035bd2835e714e2a
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-16 19:55:03 -0500

    commit to separe the branches master with files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 console.log("THIS IS THE BRANCH MASTER ");
-console.log("IN THIS BRANCH I WILL DO NOTHING :p"); 
+console.log("IN THIS BRANCH I WILL DO NOTHING ");
+
+console.log("this is just a new console log to separe the branches :p")
