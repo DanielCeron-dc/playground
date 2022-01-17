b9f677c74a1392e50dff488bfac8accfeda8ced8
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-16 19:41:22 -0500

    index added

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as files from './tools/files';
+
+const filefunction = async (): Promise<void> => {
+
+	files.createFile("files/test.txt", "Hello World!  \n dasdaddaa dads");
+	const file = await files.readFiletxt("test.txt");
+	console.log(file);
+
+}
+
+filefunction();
