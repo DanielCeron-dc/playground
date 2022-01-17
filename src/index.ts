8bac00cba9f755656a509a6e011ead4617a60589
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2022-01-16 19:55:49 -0500

    console.log added

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import * as files from './tools/files';
 
 const filefunction = async (): Promise<void> => {
 
-	files.createFile("files/test.txt", "Hello World!  \n dasdaddaa dads");
-	const file = await files.readFiletxt("test.txt");
-	console.log(file);
+    files.createFile("files/test.txt", "Hello World!  \n dasdaddaa dads");
+    const file = await files.readFiletxt("test.txt");
+    console.log(file);
+    console.log("Hello World!");
 
 }
 
